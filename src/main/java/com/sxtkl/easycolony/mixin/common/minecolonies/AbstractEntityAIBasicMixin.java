5282f2ecbf0a1bd44190e886b26809a3c56fb298
sxt <2735950909@qq.com>
2025-02-11 00:48:40 +0800

    修复了有时市民没有货却不发布缺少物品请求的漏洞

--- src/main/java/com/sxtkl/easycolony/mixin/common/minecolonies/AbstractEntityAIBasicMixin.java
+++ src/main/java/com/sxtkl/easycolony/mixin/common/minecolonies/AbstractEntityAIBasicMixin.java
@@ -1,11 +1,13 @@
 package com.sxtkl.easycolony.mixin.common.minecolonies;
 
+import com.minecolonies.api.util.Tuple;
 import com.minecolonies.core.colony.buildings.AbstractBuilding;
 import com.minecolonies.core.colony.jobs.AbstractJob;
 import com.minecolonies.core.entity.ai.workers.AbstractAISkeleton;
 import com.minecolonies.core.entity.ai.workers.AbstractEntityAIBasic;
 import com.sxtkl.easycolony.Config;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,12 +15,18 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import java.util.function.Predicate;
+
 @Mixin(AbstractEntityAIBasic.class)
+@SuppressWarnings({"DataFlowIssue"})
 public abstract class AbstractEntityAIBasicMixin<J extends AbstractJob<?, J>, B extends AbstractBuilding> extends AbstractAISkeleton<J> {
 
     @Final
     @Shadow(remap = false)
     public B building;
+
+    @Shadow(remap = false)
+    protected Tuple<Predicate<ItemStack>, Integer> needsCurrently;
 
     protected AbstractEntityAIBasicMixin(@NotNull J job) {
         super(job);
@@ -26,8 +34,9 @@ public abstract class AbstractEntityAIBasicMixin<J extends AbstractJob<?, J>, B 
 
     // 修改工人取货逻辑，使其在工作方块处取货
     @ModifyVariable(method = "getNeededItem", at = @At("STORE"), ordinal = 0, remap = false)
-    private BlockPos getNeedItem$pos(BlockPos value) {
-        if (!Config.easyPickMaterialAI) return value;
-        return building.getTileEntity().getTilePos();
+    private BlockPos getNeedItem$pos(BlockPos pos) {
+        if (!Config.easyPickMaterialAI) return pos;
+        BlockPos ori = building.getTileEntity().getPositionOfChestWithItemStack(needsCurrently.getA());
+        return ori == null ? null : building.getTileEntity().getTilePos();
     }
 }
