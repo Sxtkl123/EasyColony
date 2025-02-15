35ec4eb1ab4e1d4b6d9364e16bd8b35c8fa7a2fa
sxt <2735950909@qq.com>
2025-02-15 13:52:59 +0800

    添加更新日志静态托管测试

--- src/main/java/com/sxtkl/easycolony/mixin/common/minecolonies/AbstractEntityAIBasicMixin.java
+++ src/main/java/com/sxtkl/easycolony/mixin/common/minecolonies/AbstractEntityAIBasicMixin.java
@@ -1,13 +1,11 @@
 package com.sxtkl.easycolony.mixin.common.minecolonies;
 
-import com.minecolonies.api.util.Tuple;
 import com.minecolonies.core.colony.buildings.AbstractBuilding;
 import com.minecolonies.core.colony.jobs.AbstractJob;
 import com.minecolonies.core.entity.ai.workers.AbstractAISkeleton;
 import com.minecolonies.core.entity.ai.workers.AbstractEntityAIBasic;
 import com.sxtkl.easycolony.Config;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,18 +13,12 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import java.util.function.Predicate;
-
 @Mixin(AbstractEntityAIBasic.class)
-@SuppressWarnings({"DataFlowIssue"})
 public abstract class AbstractEntityAIBasicMixin<J extends AbstractJob<?, J>, B extends AbstractBuilding> extends AbstractAISkeleton<J> {
 
     @Final
     @Shadow(remap = false)
     public B building;
-
-    @Shadow(remap = false)
-    protected Tuple<Predicate<ItemStack>, Integer> needsCurrently;
 
     protected AbstractEntityAIBasicMixin(@NotNull J job) {
         super(job);
@@ -36,7 +28,6 @@ public abstract class AbstractEntityAIBasicMixin<J extends AbstractJob<?, J>, B 
     @ModifyVariable(method = "getNeededItem", at = @At("STORE"), ordinal = 0, remap = false)
     private BlockPos getNeedItem$pos(BlockPos pos) {
         if (!Config.easyPickMaterialAI) return pos;
-        BlockPos ori = building.getTileEntity().getPositionOfChestWithItemStack(needsCurrently.getA());
-        return ori == null ? null : building.getTileEntity().getTilePos();
+        return pos == null ? null : building.getTileEntity().getTilePos();
     }
 }
