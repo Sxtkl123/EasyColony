ab430b2c045fba862f2bc2a6847ddd73a662599f
sxt <2735950909@qq.com>
2025-02-21 19:08:17 +0800

    移除了测试方块Test

--- src/main/java/com/sxtkl/easycolony/api/block/ModBlocks.java
+++ src/main/java/com/sxtkl/easycolony/api/block/ModBlocks.java
@@ -1,6 +1,5 @@
 package com.sxtkl.easycolony.api.block;
 
 public final class ModBlocks {
-    public static TestBlock testBlock;
     public static AbstractCitizenSensorBlock citizenSensorBlock;
 }
--- src/main/java/com/sxtkl/easycolony/apiimpl/initializer/block/ModBlocksInitializer.java
+++ src/main/java/com/sxtkl/easycolony/apiimpl/initializer/block/ModBlocksInitializer.java
@@ -2,7 +2,6 @@ package com.sxtkl.easycolony.apiimpl.initializer.block;
 
 import com.sxtkl.easycolony.Easycolony;
 import com.sxtkl.easycolony.api.block.ModBlocks;
-import com.sxtkl.easycolony.api.block.TestBlock;
 import com.sxtkl.easycolony.core.block.CitizenSensorBlock;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -31,12 +30,10 @@ public final class ModBlocksInitializer {
     }
 
     public static void init(final IForgeRegistry<Block> registry) {
-        ModBlocks.testBlock = new TestBlock().registerBlock(registry);
         ModBlocks.citizenSensorBlock = new CitizenSensorBlock().registerBlock(registry);
     }
 
     public static void registerBlockItem(final IForgeRegistry<Item> registry) {
-        ModBlocks.testBlock.registerBlockItem(registry, new Item.Properties());
         ModBlocks.citizenSensorBlock.registerBlockItem(registry, new Item.Properties());
     }
 
