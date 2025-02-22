0b6f4f496584972fc57aa836833a677a876505a6
sxt <2735950909@qq.com>
2025-02-22 18:19:05 +0800

    修改为abstract

--- src/main/java/com/sxtkl/easycolony/mixin/client/minecolonies/ItemListModuleWindowMixin.java
+++ src/main/java/com/sxtkl/easycolony/mixin/client/minecolonies/ItemListModuleWindowMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemListModuleWindow.class)
-public class ItemListModuleWindowMixin {
+public abstract class ItemListModuleWindowMixin {
 
     @Redirect(method = "lambda$updateResources$3", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"), remap = false)
     private boolean updateResources$filterPredicate(String instance, CharSequence s) {
