9bcec251e5af646bb2b8528ed7cdd23f62463fa6
sxt <2735950909@qq.com>
2025-03-02 23:40:49 +0800

    初步添加了资源卷轴的方块，啥功能都没做呢

--- src/main/java/com/sxtkl/easycolony/api/block/AbstractResourceScrollBlock.java
+++ src/main/java/com/sxtkl/easycolony/api/block/AbstractResourceScrollBlock.java
@@ -1,20 +1,75 @@
 package com.sxtkl.easycolony.api.block;
 
 import com.sxtkl.easycolony.Easycolony;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.registries.IForgeRegistry;
 
+@SuppressWarnings({"deprecation", "NullableProblems"})
 public abstract class AbstractResourceScrollBlock extends Block {
     private static final String NAME = "resource_scroll";
 
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    protected static final VoxelShape EAST_AABB = Block.box(0.0D, 0.0D, 0.0D, 3.0D, 16.0D, 16.0D);
+    protected static final VoxelShape WEST_AABB = Block.box(13.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape SOUTH_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 3.0D);
+    protected static final VoxelShape NORTH_AABB = Block.box(0.0D, 0.0D, 13.0D, 16.0D, 16.0D, 16.0D);
+
     public AbstractResourceScrollBlock() {
-        super(Properties.copy(Blocks.OAK_SIGN));
+        super(Properties.copy(Blocks.LADDER).noCollission());
+        this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
+    }
+
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return switch (pState.getValue(FACING)) {
+            case NORTH -> NORTH_AABB;
+            case SOUTH -> SOUTH_AABB;
+            case WEST -> WEST_AABB;
+            default -> EAST_AABB;
+        };
+    }
+
+    private boolean canAttachTo(BlockGetter pBlockReader, BlockPos pPos, Direction pDirection) {
+        BlockState blockstate = pBlockReader.getBlockState(pPos);
+        return blockstate.isFaceSturdy(pBlockReader, pPos, pDirection);
+    }
+
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        Direction direction = pState.getValue(FACING);
+        return this.canAttachTo(pLevel, pPos.relative(direction.getOpposite()), direction);
     }
 
     public AbstractResourceScrollBlock registerBlock(final IForgeRegistry<Block> registry) {
         registry.register(new ResourceLocation(Easycolony.MODID, NAME), this);
         return this;
+    }
+
+    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
+        if (pFacing.getOpposite() == pState.getValue(FACING) && !pState.canSurvive(pLevel, pCurrentPos)) {
+            return Blocks.AIR.defaultBlockState();
+        }
+        return super.updateShape(pState, pFacing, pFacingState, pLevel, pCurrentPos, pFacingPos);
+    }
+
+    public BlockState rotate(BlockState pState, Rotation pRotation) {
+        return pState.setValue(FACING, pRotation.rotate(pState.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState pState, Mirror pMirror) {
+        return pState.rotate(pMirror.getRotation(pState.getValue(FACING)));
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
     }
 }
--- src/main/java/com/sxtkl/easycolony/core/block/ResourceScrollBlock.java
+++ src/main/java/com/sxtkl/easycolony/core/block/ResourceScrollBlock.java
@@ -3,4 +3,9 @@ package com.sxtkl.easycolony.core.block;
 import com.sxtkl.easycolony.api.block.AbstractResourceScrollBlock;
 
 public class ResourceScrollBlock extends AbstractResourceScrollBlock {
+
+    public ResourceScrollBlock() {
+        super();
+    }
+
 }
