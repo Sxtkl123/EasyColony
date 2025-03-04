50b70d39270e0cf930ff1e586e8d2d56ab69f53e
sxt <2735950909@qq.com>
2025-03-04 23:03:12 +0800

    优化import

--- src/main/java/com/sxtkl/easycolony/mixin/common/minecolonies/BuildingStructureHandlerMixin.java
+++ src/main/java/com/sxtkl/easycolony/mixin/common/minecolonies/BuildingStructureHandlerMixin.java
@@ -5,19 +5,13 @@ import com.minecolonies.api.util.InventoryUtils;
 import com.minecolonies.core.entity.ai.workers.AbstractEntityAIStructure;
 import com.minecolonies.core.entity.ai.workers.util.BuildingStructureHandler;
 import com.sxtkl.easycolony.core.colony.buildings.modules.BuildingModules;
-import com.sxtkl.easycolony.core.colony.buildings.modules.ConsumeStatsModule;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.List;
 
 @Mixin(BuildingStructureHandler.class)
 public abstract class BuildingStructureHandlerMixin {
